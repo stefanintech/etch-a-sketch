1c8110b1987f7fda463570a165c8af3b0673fee0
Stefan Taitano <stefantaitano@gmail.com>
2026-05-03 08:55:44 -0500

    Updated js script to create boxes dynamically based on number inputted

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector("#container");
 
 function createBox(num) {
+    let size = 960 / Math.sqrt(num)
     for (let index = 0; index < num; index++) {
         const div = document.createElement("div");
-        div.classList = "box"
+        div.style.width = size + "px"
+        div.style.height = size + "px"
+        div.classList.toggle("box")
         container.appendChild(div)
     }
 }
 
-createBox(16);
+createBox(256);
